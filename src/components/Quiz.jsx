cf31ad452da0771aa1ba5e47cb00a9fbbee8a7bd
Roshni Mishra <mishraroshni2004@gmail.com>
2025-08-23 14:06:23 +0530

    Added Quiz Logic and Question Timers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import QUESTIONS from "../questions.js"
-
+import QuestionTimer from "./QuestionTimer.jsx";
+import quizCompleteImg from "../assets/quiz-complete.png"
 
 export default function Quiz(){
     
@@ -12,14 +13,37 @@ export default function Quiz(){
 
     const activeQuestionIndex = userAnswers.length;
 
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
      function handleSelectAnswer(selectedAnswer){
      setUserAnswers((prevUserAnswers) => {
          return [...prevUserAnswers, selectedAnswer];   
      });
      }
+
+     if(quizIsComplete){
+        return (
+        <div id="summary">
+          <img src={quizCompleteImg} alt="Quiz Completed"/>
+          <h2>Quiz Completed</h2>
+        </div>
+        );
+     }
+
+     //creating new array(copy) so that don't edit the original array
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+
+   // sort will not return new array but instead edit the array
+   // if -ve no. -1 then those elements will be swapped 
+   // if +ve no. they will stay in the order they are
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
     return (
      <div id= "quiz">
          <div id="questions">
+            <QuestionTimer timeout={10000}
+             onTimeout={() => handleSelectAnswer(null)}
+              />
     <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
      <ul id="answers">
         {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
